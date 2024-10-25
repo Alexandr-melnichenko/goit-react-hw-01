6172a26549a19466f1915ba127b2d8d92d8806a2
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-10-25 07:13:04 +0300

    ready hw

---
+++ src/Components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,31 @@
+import s from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+      <table className={s.table}>
+        <thead >
+          <tr className={s.tableHead}>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+            {items.map((item) => {
+                return (
+                <tr key={item.id}>
+                    <td className={s.alignColumn}>{item.type}</td>
+                    <td className={s.alignColumn}>{item.amount}</td>
+                    <td>{item.currency}</td>
+                  </tr>)
+            })}
+          
+          
+        </tbody>
+      </table>
+    
+  );
+};
+
+export default TransactionHistory;
